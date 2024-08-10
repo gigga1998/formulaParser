82bcf800fb69272b0e538a76fbbc11a53cc68074
Igor Berdnikov <igor.berdnikov@sofoil.com>
2024-08-10 16:26:36 +0300

    разделение выражеия на коэффициенты и мономы

---
+++ formulaParser.go
@@ -0,0 +1,76 @@
+package formulaParser
+
+import (
+	"strings"
+	"unicode"
+)
+
+func SeparateBrackets(formula string) []string {
+	/*
+		(ax + b)(cx + d) -> acx2 + (ad + bc)x + bd
+	*/
+	var output = []string{}
+	var statement = ""
+
+	for _, ch := range formula {
+		if ch == '(' {
+			statement = ""
+		} else if ch == ')' {
+			output = append(output, statement)
+		} else {
+			statement += string(ch)
+		}
+
+	}
+	return output
+}
+
+func SeparatePair(pair string) [3]string {
+	/*
+		"ax + b" -> {a, b, x}
+	*/
+	trimString := strings.ReplaceAll(pair, " ", "")
+	firstCoef := ""
+	secondCoef := ""
+	variable := ""
+	i := 0
+
+	for _, ch := range trimString {
+		if digitOrOperator(ch) {
+			firstCoef += string(ch)
+			i++
+		} else {
+			break
+		}
+	}
+
+	trimString = trimString[i:]
+	i = 0
+	for _, ch := range trimString {
+		if !digitOrOperator(ch) {
+			variable += string(ch)
+			i++
+		} else {
+			break
+		}
+	}
+
+	trimString = trimString[i:]
+	for _, ch := range trimString {
+		if digitOrOperator(ch) {
+			secondCoef += string(ch)
+			i++
+		} else {
+			break
+		}
+	}
+	out := [3]string{firstCoef, secondCoef, variable}
+	return out
+}
+
+func digitOrOperator(ch rune) bool {
+	if unicode.IsDigit(ch) || ch == '-' || ch == '+' {
+		return true
+	}
+	return false
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gigga1998/formulaParser
+
+go 1.22.6
